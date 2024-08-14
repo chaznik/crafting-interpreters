af971302a393802deadc1966511012002cfaed58
Chazni Katz <chazni.k9@gmail.com>
2024-08-14 02:23:59 +0200

    basic single char lexing and error logging

--- cmd/glox/main.go
+++ cmd/glox/main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
-const (
-	LEFT_PAREN  = "("
-	RIGHT_PAREN = ")"
-	LEFT_BRACE  = "{"
-	RIGHT_BRACE = "}"
-	COMMA       = ","
-	DOT         = "."
-	MINUS       = "-"
-	PLUS        = "+"
-	SEMICOLON   = ";"
-	SLASH       = "/"
-	STAR        = "*"
-)
+var hasError bool = false
+
+type Token struct {
+	tokenType string
+	lexeme    string
+	literal   string
+	line      int
+}
+
+func newToken(tokenType string, lexeme string, literal string, line int) *Token {
+	t := Token{tokenType: tokenType, lexeme: lexeme, literal: literal, line: line}
+	return &t
+}
+
+func (t Token) toString() string {
+	return t.tokenType + " " + t.lexeme + " " + t.literal
+}
+
+var hadError bool = false
 
 func main() {
-
 	input := os.Args
 
 	if len(input) > 3 {
@@ -41,7 +48,6 @@ func main() {
 }
 
 func runFile(filePath string) {
-
 	data, err := os.ReadFile(filePath)
 
 	if err != nil {
@@ -49,52 +55,97 @@ func runFile(filePath string) {
 		os.Exit(1)
 	}
 
-	var errors []string
-	var lexemes []string
+	run(string(data))
 
-	for _, char := range string(data) {
-
-		charString := string(char)
-
-		switch charString {
-		case LEFT_PAREN:
-			lexemes = append(lexemes, charString)
-		case RIGHT_PAREN:
-			lexemes = append(lexemes, charString)
-		case LEFT_BRACE:
-			lexemes = append(lexemes, charString)
-		case RIGHT_BRACE:
-			lexemes = append(lexemes, charString)
-		case COMMA:
-			lexemes = append(lexemes, charString)
-		case DOT:
-			lexemes = append(lexemes, charString)
-		case MINUS:
-			lexemes = append(lexemes, charString)
-		case PLUS:
-			lexemes = append(lexemes, charString)
-		case SEMICOLON:
-			lexemes = append(lexemes, charString)
-		case SLASH:
-			lexemes = append(lexemes, charString)
-		case STAR:
-			lexemes = append(lexemes, charString)
-		default:
-			errors = append(errors, charString)
-		}
-	}
-
-	lexemes = append(lexemes, "EOF")
-
-	for _, error := range errors {
-		fmt.Fprintf(os.Stderr, "[line 1] Error: Unexpected character: %s\n", error)
-	}
-
-	for _, lexeme := range lexemes {
-		fmt.Println(lexeme)
+	if hadError {
+		os.Exit(65)
 	}
 }
 
 func runPrompt() {
-	fmt.Println("Running prompt")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		line, _ := reader.ReadString('\n')
+		run(line)
+		hadError = false
+	}
+}
+
+func run(source string) {
+	var tokens []string
+	var errors []string
+	line := 1
+
+	for _, char := range source {
+
+		charString := string(char)
+
+		switch charString {
+		case "(":
+			tokens = append(tokens, "LEFT_PAREN"+" "+charString+" "+"null")
+			break
+		case ")":
+			tokens = append(tokens, "RIGHT_PAREN"+" "+charString+" "+"null")
+			break
+		case "{":
+			tokens = append(tokens, "LEFT_BRACE"+" "+charString+" "+"null")
+			break
+		case "}":
+			tokens = append(tokens, "RIGHT_BRACE"+" "+charString+" "+"null")
+			break
+		case ",":
+			tokens = append(tokens, "COMMA"+" "+charString+" "+"null")
+			break
+		case ".":
+			tokens = append(tokens, "DOT"+" "+charString+" "+"null")
+			break
+		case "-":
+			tokens = append(tokens, "MINUS"+" "+charString+" "+"null")
+			break
+		case "+":
+			tokens = append(tokens, "PLUS"+" "+charString+" "+"null")
+			break
+		case ";":
+			tokens = append(tokens, "SEMICOLON"+" "+charString+" "+"null")
+			break
+		case "/":
+			tokens = append(tokens, "SLASH"+" "+charString+" "+"null")
+			break
+		case "*":
+			tokens = append(tokens, "STAR"+" "+charString+" "+"null")
+			break
+		case "\n":
+			line++
+			break
+		default:
+			hasError = true
+			errors = append(errors, "[line "+strconv.Itoa(line)+"] Error: Unexpected character: "+charString)
+		}
+	}
+
+	for _, err := range errors {
+		fmt.Fprint(os.Stderr, "\n", err)
+	}
+
+	for _, token := range tokens {
+		fmt.Println(token)
+	}
+
+	fmt.Println("EOF  null")
+
+	if hasError {
+		os.Exit(65)
+	}
+}
+
+func error(line int, message string) {
+	report(line, "", message)
+}
+
+func report(line int, where string, message string) {
+	fmt.Fprintf(os.Stderr, "[line %d ] Error %s : %s", line, where, message)
+	hadError = true
 }
